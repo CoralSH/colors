c111b025e22e6c44b31d1586072685bd06ac7305
Nimit Kalra <nimit@nimitkalra.com>
2015-12-23 01:38:53 -0600

    Add regular colors.

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,3 +1,8 @@
-turquoise() {
-  printf "\033[36m$1\033[0m"
-}
+black() { printf "\e[${2:-0};${3:-30}m$1\e[0m"; }
+red() { printf "\e[${2:-0};${3:-31}m$1\e[0m"; }
+greeen() { printf "\e[${2:-0};${3:-32}m$1\e[0m"; }
+yellow() { printf "\e[${2:-0};${3:-33}m$1\e[0m"; }
+blue() { printf "\e[${2:-0};${3:-34}m$1\e[0m"; }
+purple() { printf "\e[${2:-0};${3:-35}m$1\e[0m"; }
+cyan() { printf "\e[${2:-0};${3:-36}m$1\e[0m"; }
+white() { printf "\e[${2:-0};${3:-37}m$1\e[0m"; }
